8152bc7b4ea79ac370aac01f0bba460d110e3d7f
Acasune <tttt4aaaa@gmail.com>
2022-11-21 08:54:57 +0900

    add interfaces

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use base64ct::{Base64, Encoding};
 use serde::{Deserialize, Serialize};
 
 #[get("/")]
@@ -9,18 +10,30 @@ async fn index() -> Result<HttpResponse, actix_web::Error> {
 
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
-    HttpServer::new(move || App::new().service(index).service(post_url))
+    HttpServer::new(move || App::new().service(get_url).service(post_url))
         .bind("0.0.0.0:8080")?
         .run()
         .await?;
     Ok(())
 }
 
+#[get("/{hashed_url}")]
+async fn get_url(hashed_url: web::Path<String>) -> impl Responder {
+    let res = format!("{}", hashed_url.as_str());
+    HttpResponse::Ok().body(res)
+}
+
 #[post("/")]
 async fn post_url(data: web::Json<PostData>) -> impl Responder {
-    println!("{:?}", data);
-    HttpResponse::Ok().body(data.0.user_name)
+    let hashed = Base64::encode_string(data.0.url.as_bytes());
+    HttpResponse::Ok().body(hashed)
 }
+
+// #[delete("/{hashed_url}")]
+// async fn delete_url(hashed_url: web::Path<String>) -> impl Responder {
+//     let res = format!("{}", hashed_url.as_str());
+//     HttpResponse::Ok().body(res)
+// }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct PostData {
